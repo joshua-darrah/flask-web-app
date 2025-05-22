8a0efa96b5b97a82e03adce47530b064d2ec8510
unknown <darrahjoshua551@gmail.com>
2025-05-22 23:35:39 +0000

    Updated files

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return '<p>This is your login page</p>'
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -12,4 +13,4 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    return '<p>Sign Up here</p>'
+    return render_template("sign_up.html")
