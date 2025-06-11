73f2e19cd5500ccf1a3bce953f6e3eef26663eaf
Joshua Darrah <darrahjoshua551@gmail.com>
2025-06-11 22:06:47 +0000

    Updated

--- website/models.py
+++ website/models.py
@@ -1,11 +1,13 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from datetime import datetime
 
 class Note(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    # date = db.Column(db.DateTime(timezone=True), default=func.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
