9f290afa4e487a684a43f7d7369bf3feb8d2bc28
Joshua Darrah <darrahjoshua551@gmail.com>
2025-06-11 20:51:57 +0000

    Updated

--- website/auth.py
+++ website/auth.py
@@ -22,7 +22,7 @@ def login():
                 flash("Incorrect Password! Try Again", category='error')
         else:
             flash('Email does not exist!', category='error')
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -58,4 +58,4 @@ def sign_up():
             return redirect(url_for('views.home'))
     
         
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
